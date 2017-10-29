6d1805bff216a13a4174f48de2d4eb44be2f50c7
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2017-10-29 01:20:52 -0700

    Add files via upload

--- Code/color_LCD_v4.ino
+++ Code/color_LCD_v4.ino
@@ -1,7 +1,39 @@
-//Version 4 (detects yellow in addition to R, G, B, and Power off (no color))
+/*
+ color_LCD version 4
+ Integrates the RGB sensor, the LCD and speaker actuators, and the SD card module
+ 10/28/2017
+ 
+ Simple SD Audio example, shows the usage of most functions 
+ via terminal at serial communication port.
+ 
+ This example shows how to use the SimpleSDAudio library for audio playback.
+ You need: 
+ - An Arduino with ATmega368 or better
+ - An SD-Card connected to Arduinos SPI port (many shields will do)
+   -> copy some converted audio files on freshly formated SD card into root folder
+ - A passive loudspeaker and resistor or better: active speakers (then stereo output will be possible)
+ 
+ Audio signal output is at the following pin:
+ - Arduino with ATmega328       (many non-mega Arduinos): Pin 9
+ - Arduino with ATmega1280/2560 (many mega Arduinos)    : Pin 44
+ 
+ Using passive speaker:  
+    Audio-Pin --- -[100 Ohm resistor]- ---- Speaker ---- GND
+    
+ Using amplifier / active speaker / line-in etc.:
+    Audio-Pin --||--------------[10k resistor]----+----[1k resistor]---- GNDm
+              100nF capacitor                   to amp
+ 
+ See SimpleSDAudio.h or our website for more information:
+ http://www.hackerspace-ffm.de/wiki/index.php?title=SimpleSDAudio
+ 
+ created  29 Jun 2012 by Lutz Lisseck
+ */
+
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
 #include <LiquidCrystal.h>
+#include <SimpleSDAudio.h>
 
 // Pick analog outputs, for the UNO these three work well
 // use ~560  ohm resistor between Red & Blue, ~1K for green (its brighter)
@@ -16,30 +48,34 @@
 
 // our RGB -> eye-recognized gamma color
 byte gammatable[256];
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2, FILE_SIZE = 20;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// Create static buffer 
+#define BIGBUFSIZE (2*512)      // bigger than 2*512 is often only possible on Arduino megas!
+uint8_t bigbuf[BIGBUFSIZE];
 
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 
 void setup() {
+  // Setting the buffer manually for more flexibility (speaker initialization code)
   
+  //LCD initialization code
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   Serial.begin(9600);
-  Serial.println("Color View Test!");
-  lcd.noAutoscroll();  
-  if (tcs.begin()) {
-    Serial.println("Found sensor");
-  } else {
-    Serial.println("No TCS34725 found ... check your connections");
-    while (1); // halt!
-  }
+  lcd.noAutoscroll();
+  tcs.begin();
   
+  SdPlay.setWorkBuffer(bigbuf, BIGBUFSIZE);
+ 
+  SdPlay.init(SSDA_MODE_FULLRATE | SSDA_MODE_MONO | SSDA_MODE_AUTOWORKER) ; 
   // use these three pins to drive an LED
   pinMode(redpin, OUTPUT);
   pinMode(greenpin, OUTPUT);
   pinMode(bluepin, OUTPUT);
-  
+
+  //RGB initialization code
   // thanks PhilB for this gamma table!
   // it helps convert RGB colors to what humans see
   for (int i=0; i<256; i++) {
@@ -53,13 +89,14 @@ void setup() {
     } else {
       gammatable[i] = x;      
     }
-    //Serial.println(gammatable[i]);
   }
 }
 
-
 void loop() {
+  //Variables for the RGB library
   uint16_t clear, red, green, blue;
+  //Variables for the audio library
+  uint8_t count=0, c, flag;
 
   tcs.setInterrupt(false);      // turn on LED
 
@@ -68,11 +105,6 @@ void loop() {
   tcs.getRawData(&red, &green, &blue, &clear);
 
   tcs.setInterrupt(true);  // turn off LED
-  
-  Serial.print("C:\t"); Serial.print(clear);
-  Serial.print("\tR:\t"); Serial.print(red);
-  Serial.print("\tG:\t"); Serial.print(green);
-  Serial.print("\tB:\t"); Serial.print(blue);
 
   // Figure out some basic hex code for visualization
   uint32_t sum = clear;
@@ -81,20 +113,26 @@ void loop() {
   g = green; g /= sum;
   b = blue; b /= sum;
   r *= 256; g *= 256; b *= 256;
-  Serial.print("\t");
-  Serial.print((int)r, HEX); Serial.print((int)g, HEX); Serial.print((int)b, HEX);
-  Serial.println();
+
   // Print a message to the LCD.
   if(clear < 800)
+    {
     lcd.print("Power OFF");
+    }
   else if(r > b & g > b)
     lcd.print("yellow");
   else if(r > g & r > b)
     lcd.print("red");
   else if(g > r & g > b)
     lcd.print("green");
-  else if(b > r || b > g)
+  else if(b > r || b > g){
     lcd.print("blue");
+    delay(1000);
+    SdPlay.setFile("SQZUP2.WAV");
+    SdPlay.play();
+    lcd.clear();
+    delay(4000);
+    }
   else
     lcd.print("error");
   //Serial.print((int)r ); Serial.print(" "); Serial.print((int)g);Serial.print(" ");  Serial.println((int)b );
